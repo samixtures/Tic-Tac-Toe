28e73186c347b0af1e190021b1bf6dc85a1a3f2b
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-07-18 16:42:25 -0700

    chec_set_winner method checks all 3s in a row now

--- ttt.py
+++ ttt.py
@@ -2,6 +2,10 @@ class Player:
     def __init__(self, symbol):
         # X or O
         self.symbol = symbol
+# class PlayerO(Player):
+#     def __init__(self, symbol):
+#         super().__init__()
+        
 class Game:
     def __init__(self):
         # doing 'str' because when we do '.join()' in
@@ -14,22 +18,31 @@ class Game:
     def move(self, value, position): #value being X or O and position being 0-8
         self.board[position] = value
     def check_set_winner(self, symbol):
-        if self.board[0] == self.board[1] == self.board[2]:
-            #OR 0 == 3 == 6
-            #OR 0 == 4 == 8
-            #OR 2 == 5 == 8
-            #OR 6 == 7 == 8
+        #THIS CHECKS 0, 3, 6; 1, 4, 7; 2, 5, 8
+        for i in range(3):
+            if self.board[i]==self.board[i+3]==self.board[i+6]:
+                self.winner = symbol
+        #THIS CHECKS 0, 1, 2; 3, 4, 5; 6, 7, 8
+        for i in range(0,7,3): #0-8
+            if self.board[i]==self.board[i+1]==self.board[i+2]:
+                self.winner = symbol
+        #THIS CHECKS 0, 4, 8
+        if self.board[0]==self.board[4]==self.board[8]:
             self.winner = symbol
 
+            #[0, 1, 2, 3, 4, 5, 6, 7, 8]
+            # | 0 | 1 | 2 |
+            # | 3 | 4 | 5 |
+            # | 6 | 7 | 8 |
+                    
 # |  |  |  |
 # |  |  |  |
 # |  |  |  |
 p1 = Player('O')
 g = Game()
-g.move(p1.symbol, 2)
 g.display_game()
 while not g.winner:
-    move_position = input("Your move? Type a # from 0 - 8: ")
+    move_position = input("Your move, type a # from 0 - 8: ")
     g.move(p1.symbol, int(move_position))
     g.display_game()
     g.check_set_winner(p1.symbol)
